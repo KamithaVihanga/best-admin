3db300eb1a7c1ca06e95ee6a715b81272f0b02fd
Kamitha Bodhinayake <kvihanga@gmail.com>
2019-04-28 09:20:57 +0530

    initial commit

---
+++ src/app/add-tutor/add-tutor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tutor',
+  templateUrl: './add-tutor.component.html',
+  styleUrls: ['./add-tutor.component.css']
+})
+export class AddTutorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {TutorsComponent} from './tutors/tutors.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import { AddTutorComponent } from './add-tutor/add-tutor.component';
+import { StudentComponent } from './student/student.component';
+
+const routes: Routes = [{
+  path: '',
+  redirectTo: '/dashboard',
+  pathMatch: 'full'
+},{
+  path: "dashboard",
+  component: DashboardComponent
+}, {
+  path: "tutors",
+  component: TutorsComponent
+},{
+  path: "add-tutor",
+  component: AddTutorComponent
+},{
+  path:"students",
+  component:StudentComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TutorsComponent,
+    DashboardComponent,
+    AddTutorComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
